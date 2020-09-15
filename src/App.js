88575f78f604a1e749b1c3790df8b814586e704e
amine reda <amine.reda037@gmail.com>
2020-09-15 12:49:55 +0100

    add header part

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from "react";
 
 import "./App.css";
+import Header from "./Components/Header";
 
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <Header />
+    </div>
+  );
 }
 
 export default App;
