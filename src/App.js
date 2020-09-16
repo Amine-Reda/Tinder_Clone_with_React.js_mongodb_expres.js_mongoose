e14dc106d404464ac828d6af3686113892998323
amine reda <amine.reda037@gmail.com>
2020-09-16 12:47:43 +0100

    add swipe buttons

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import React from "react";
 import "./App.css";
 import Header from "./Components/Header";
 import TinderCards from "./Components/TinderCards";
-
+import SwipeButtons from "./Components/SwipeButtons";
 function App() {
   return (
     <div className="app">
       <Header />
       <TinderCards />
+      <SwipeButtons />
     </div>
   );
 }
